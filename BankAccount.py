2714ecb7ba55162ece9138055ef4e0124008b2a8
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-09 02:48:55 -0800

    Created BankAccount class

---
+++ BankAccount.py
@@ -0,0 +1,6 @@
+class BankAccount:
+    def __init__(self, full_name, account_number, routing_number, balance):
+        self.full_name = str
+        self.account_number = int
+        self.routing_number = int
+        self.balace = int 
