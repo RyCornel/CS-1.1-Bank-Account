06a22449cdbed56109c85a433447d5cf1c665a07
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-11 20:29:47 -0800

    Updated & Working.
    Obtained help from cohort Salomon Cohen.

--- BankAccount.py
+++ BankAccount.py
@@ -1,36 +1,47 @@
+from random import random
+
 class BankAccount:
-    def __init__(self, full_name, account_number, routing_number, balance):
-        self.full_name = str
-        self.account_number = int
+    def __init__(self, full_name):
+        self.full_name = full_name
+        self.account_number = random(1000000, 9999999)
         self.routing_number = int
-        self.balance = int 
+        self.balance = 0.00
 
     def deposit(self, amount):
-        self.balance + amount
+        self.balance += amount
+        print(f"Deposit Amount: ${amount}")
+        print(f"Updated Balance: ${self.balance}")
         return self.balance
 
     def withdraw(self, amount):
-        if amount > self.balance:
-            return ("INVALID TRANSACTION")
-        self.balance - amount
-        print("Amount Withdrawn: ${amount}")
+        if self.balance >= amount:
+            self.balance -= amount
+            print(f"Withdrawl Amount: ${amount}")
+            print(f"Updated Balance: ${self.balance}")
+        else:
+            print(f"Insufficient Funds: ${self.balance}")
+            self.balance -= + 10
+            print(f"New Balance: ${self.balance}")
+
+    def get_balance(self):
+        print(f"Current Account Balance: $self.balance")
         return self.balance
 
-    def get_balance(self, balance):
-        print(self.balance)
-        return self.balance
-
-    def add_interest(self, balance):
-         self.balance * 0.00083 
-         return self.balance
+    def add_interest(self):
+        interest = self.balance * 0.00083 
+        self.balance += interest
+    
+    def display_num(self):
+        acct_num = range(len(str(self.account_number)))
+            
+        for i in acct_num:
+            if i <= 4:
+                self.display_num += "*"
+            else:
+                self.display_num += str(self.account_number)[i]
 
     def print_statement(self):
         print(f"Hello {self.full_name}! Your balance for account {self.account_number} is: {self.balance}. Don't forget our Routing Number: {self.routing_number}, in order to deposit or withdraw funds in the future.")
-        
-BankAccount("Ryan Cornel", 123456789, 987654321, 10000)
-BankAccount.withdraw(amount=2000)
-print(BankAccount.get_balance)
+    
 
-BankAccount("Ryan Cornel", 123456789, 987654321, 10000)
-BankAccount.deposit(amount=5000)
-print(BankAccount.get_balance)
+ryan = BankAccount("Ryan Cornel")
